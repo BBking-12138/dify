1b7f05215410104a46d5989e1f9617ad3b02ccc3
BBking-12138 <wangjunjie88888@gmail.com>
2024-10-10 16:23:34 +0800

    Update baidu.py

--- api/core/tools/provider/builtin/baidu/baidu.py
+++ api/core/tools/provider/builtin/baidu/baidu.py
@@ -1,28 +1,21 @@
-from core.tools.entities.tool_entities import ToolInvokeMessage, ToolProviderType
-from core.tools.tool.tool import Tool
-from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
+from typing import Any
 from core.tools.errors import ToolProviderCredentialValidationError
+from core.tools.provider.builtin.baidu.tools.baidu_search import BaiduSearchTool
+from core.tools.provider.builtin_tool_provider import BuiltinToolProviderController
 
-from core.tools.provider.builtin.google.tools.google_search import GoogleSearchTool
-
-from typing import Any, Dict
-
-class GoogleProvider(BuiltinToolProviderController):
-    def _validate_credentials(self, credentials: Dict[str, Any]) -> None:
+class BaiduProvider(BuiltinToolProviderController):
+    def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
-            # 1. 此处需要使用 GoogleSearchTool()实例化一个 GoogleSearchTool，它会自动加载 GoogleSearchTool 的 yaml 配置，但是此时它内部没有凭据信息
-            # 2. 随后需要使用 fork_tool_runtime 方法，将当前的凭据信息传递给 GoogleSearchTool
-            # 3. 最后 invoke 即可，参数需要根据 GoogleSearchTool 的 yaml 中配置的参数规则进行传递
-            GoogleSearchTool().fork_tool_runtime(
-                meta={
+            # 创建百度搜索工具的实例
+            BaiduSearchTool().fork_tool_runtime(
+                runtime={
                     "credentials": credentials,
                 }
             ).invoke(
-                user_id='',
-                tool_Parameters={
-                    "query": "test",
-                    "result_type": "link"
-                },
+                user_id="",
+                tool_parameters={"query": "test", "result_type": "link"},
             )
         except Exception as e:
+            # 捕获异常并抛出凭证验证错误
             raise ToolProviderCredentialValidationError(str(e))
+
