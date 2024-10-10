e2f92415c89cdbc8aa0e2fb422186bba50501adf
Bowen Liang <liangbowen@gf.com.cn>
2024-10-10 09:45:37 +0800

    update

--- api/core/tools/tool_manager.py
+++ api/core/tools/tool_manager.py
@@ -640,16 +640,10 @@ class ToolManager:
             raise ValueError(f"provider type {provider_type} not found")
 
 
-import time
-
-time1 = time.time()
-print("load_builtin_providers_cache")
-import asyncio
-
-
 async def async_preload_builtin_tool_providers():
     ToolManager.load_builtin_providers_cache()
 
 
+import asyncio
+
 asyncio.get_event_loop().create_task(async_preload_builtin_tool_providers())
-print(f"load_builtin_providers_cache time cost {str(time.time() - time1)} sec")
